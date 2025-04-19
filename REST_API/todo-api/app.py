38f03f7dc12874b6df4faa05ccba0bd0f57fce1a
Sirius1616 <siriusa1.615@gmail.com>
2025-04-19 02:49:05 +0100

    To do

--- REST_API/todo-api/app.py
+++ REST_API/todo-api/app.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python
 
 from flask import Flask, jsonify, abort, make_response, request
+from flask_httpauth import HTTPBasicAuth
 
 
 app = Flask(__name__)
+auth = HTTPBasicAuth()
 
 
 tasks = [
@@ -22,6 +24,8 @@ tasks = [
 ]
 
 
+
+
 @app.route('/todo/api/v1.0/tasks/<int:task_id>', methods = ['GET'])
 def get_task(task_id):
     task = [task for task in tasks if task['id'] == task_id]
@@ -32,7 +36,7 @@ def get_task(task_id):
     return jsonify({'tasks':task[0]})
 
 @app.route('/task/api/v1.0/tasks', methods=['POST'])
-def create_task(task):
+def create_task():
     if not request.json or 'title' not in request.json:
         abort(400)
     task = {
@@ -45,9 +49,56 @@ def create_task(task):
     tasks.append(task)
     return jsonify({'tasks': tasks}), 201
 
+
+@app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['PUT'])
+def update_task(task_id):
+    task = [task for task in tasks if task['id'] == task_id]
+    if len(task) == 0:
+        abort(404)
+    if not request.json:
+        abort(400)
+    if 'title' in request.json and type(request.json['title']) is not unicode:
+        abort(400)
+    if 'description' in request.json and type(request.json['description']) is not unicode:
+        abort(400)
+    if 'done' in request.json and type(request.json['done']) is not bool:
+        abort(400)
+    task[0]['title'] = request.json.get('title', task[0]['title'])
+    task[0]['description'] = request.json.get('description', task[0]['description'])
+    task[0]['done'] = request.json.get('done', task[0]['done'])
+
+    return jsonify({'task' : task[0]})
+
+@app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['DELETE'])
+def delete_task(task_id):
+    task = [task for task in tasks if task['id'] == task_id]
+    if len(task) == 0:
+        abort(404)
+    tasks.remove(task[0])
+
+    return jsonify('result: True')
+
+@auth.get_password
+def get_password(username):
+    if username == 'john':
+        return 'Python'
+    return None
+
+
+@auth.error_handler
+def unauthorized():
+    return make_response(jsonify({'error': 'unauthorized access'}), 401)
+
+@app.route('/todo/api/v1.0/tasks', methods=['GET'])
+@auth.login_required
+def get_tasks():
+    return jsonify({'tasks': tasks})
+
+
 @app.errorhandler(404)
 def not_found(error):
     return (make_response(jsonify({'error' : 'not found'}), 404))
 
 if __name__ == "__main__":
     app.run(debug=True)
+
