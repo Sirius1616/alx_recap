b987298ab2b7dd270050642a115164298d02907d
Sirius1616 <siriusa1.615@gmail.com>
2025-08-05 14:42:01 +0100

    months commit

--- Javascript/JS_Basics/mainjs.js
+++ Javascript/JS_Basics/mainjs.js
@@ -1,28 +1,18 @@
 #!/usr/bin/env node
 
-class Vehicle {
-    constructor(make, model){
-        this.make = make;
-        this.model = model;
+class newObject{
+    constructor(value){
+        this.value = value;
     }
 
-    start(){
-        console.log(`The ${this.make} with the model ${this.model} have started`)
-    }
-}
-
-
-class Car extends Vehicle{
-    constructor(make, model, year){
-        super(make, model)
-        this.year = year;
-    }
-    start(){
-        console.log(`This is the new vihicle I got with make of ${this.make} and model of ${this.model}`)
+    increament(){
+        this.value = this.value + 1;
+        console.log(this.value)
     }
 }
 
-const newCar =new Car("Benz", 'GLE', 2023)
+
+testObject =  new newObject(5);
 
 
-newCar.start()
+testObject.increament()
