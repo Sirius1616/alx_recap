dfd69a44c5e82bbadfa197966feb462d0664158f
Sirius1616 <siriusa1.615@gmail.com>
2025-08-10 21:27:16 +0100

    New commit

--- AirBnB_clone/ORM_test/orm_test.py
+++ AirBnB_clone/ORM_test/orm_test.py
@@ -1,6 +1,6 @@
-from sqlalchemy import text
+from sqlalchemy import text, ForeignKey
 from sqlalchemy import Column, Integer, String, create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy.orm import sessionmaker, declarative_base, relationship
 
 Base = declarative_base()
 
@@ -12,21 +12,29 @@ class User(Base):
     fullname = Column(String(48))
     nickname = Column(String(48))
 
+    posts = relationship('Post', back_populates= 'user')
+
 
     def __repr__(self):
         return "<User(name='%s', fullname='%s', nickname='%s')>" %(self.name, self.fullname, self.nickname)
+
+
+class Post(Base):
+    __tablename__ = 'posts'
+
+    id = Column(Integer, primary_key=True)
+    title = Column(String(100))
+    content = Column(String(100))
+    user_id = Column(Integer, ForeignKey('users.id'))
+
+    user = relationship('User', back_populates='posts')
 
 
 engine = create_engine('mysql+pymysql://root:Siriusa1.615@localhost:3306/mysql_db')
 
 Base.metadata.create_all(engine)
 
-Session = sessionmaker(bind=engine)
+Session = sessionmaker(bind=engine).
+
 
 session = Session()
-
-
-user = User(name='John', fullname='John Ezekiel', nickname='Sirius_A')
-session.add(user)
-
-session.commit()
