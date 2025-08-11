65a7aa24c3056837bdd719b033e19953e8c03355
Sirius1616 <siriusa1.615@gmail.com>
2025-08-11 16:13:34 +0100

    Adding many to many relationship

--- AirBnB_clone/ORM_test/orm_test.py
+++ AirBnB_clone/ORM_test/orm_test.py
@@ -1,8 +1,14 @@
-from sqlalchemy import text, ForeignKey
+from sqlalchemy import Table, text, ForeignKey
 from sqlalchemy import Column, Integer, String, create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base, relationship
 
 Base = declarative_base()
+
+user_post_association = Table('user_post_association', Base.metadata,
+                              Column('user_id', Integer, ForeignKey('users.id'), primary_key=True),
+                              Column('post_id', Integer, ForeignKey('posts.id'), primary_key=True)
+                        )
+
 
 class User(Base):
     __tablename__ = 'users'
@@ -12,7 +18,7 @@ class User(Base):
     fullname = Column(String(48))
     nickname = Column(String(48))
 
-    posts = relationship('Post', back_populates= 'user')
+    posts = relationship('Post', secondary=user_post_association, back_populates='authors')
 
 
     def __repr__(self):
@@ -25,9 +31,8 @@ class Post(Base):
     id = Column(Integer, primary_key=True)
     title = Column(String(100))
     content = Column(String(100))
-    user_id = Column(Integer, ForeignKey('users.id'))
 
-    user = relationship('User', back_populates='posts')
+    authors = relationship('User', secondary=user_post_association, back_populates='posts')
 
 
 engine = create_engine('mysql+pymysql://root:Siriusa1.615@localhost:3306/mysql_db')
