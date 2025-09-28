9cc72bb0a8f40724fd96e87affe2659df192fd9b
Sirius1616 <siriusa1.615@gmail.com>
2025-09-29 00:22:24 +0100

    request options of a server

---
+++ python_network/option_url.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/bash
+
+# Check if the second argument was passed
+if [ -z "$1"]; then
+    echo "Usage: curl <url>"
+    exit 1
+fi
+
+URL="$1"
+
+BODY=$(curl -X OPTION "$1")
+
+echo "Body of url: $BODY"
+echo 
+
+
