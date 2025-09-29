7b404a6219e62a71682ded4775b060cc59290804
Sirius1616 <siriusa1.615@gmail.com>
2025-09-29 16:35:24 +0100

    Shows http methods for a url

--- python_network/app.py
+++ python_network/app.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-from flask import Flask, jsonify, make_response
+from flask import Flask, jsonify, make_response, request
 
 
 app = Flask(__name__)
@@ -23,13 +23,14 @@ def delete_iten(item_id):
 @app.route('/items/<int:item_id>', methods=['POST'])
 def add_item(item_id):
     if item_id in items:
-        print("Item with {} already exists".format(item_id))
-        return
-    else:
-        if not item_id[0]:
-            return make_response(jsonify({'error': 'Item not added'}))
-        added = items.append(item_id)
-        return jsonify({'added': added})
+        return make_response(jsonify({'Error ': f'Item with {item_id} already exist'}), 400)
+
+    data = request.get_json()
+    if not data or 'name' not in data:
+            return make_response(jsonify({'error': 'Item not added'}), 400)
+            
+    items[item_id] = {'name': data['name']}
+    return jsonify({'added': items[item_id]}), 201
 
 
 if __name__ == "__main__":
--- python_network/option_url.sh
+++ python_network/option_url.sh
@@ -1,16 +1,17 @@
 #!/usr/bin/bash
 
 # Check if the second argument was passed
-if [ -z "$1"]; then
+if [ -z "$1" ]; then
     echo "Usage: curl <url>"
     exit 1
 fi
 
 URL="$1"
 
-BODY=$(curl -X OPTION "$1")
+BODY=$(curl -i -X OPTIONS "$1")
 
 echo "Body of url: $BODY"
-echo ""
+
+
 
 
